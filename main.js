a4c094da80057a1fa97c06ede4a950fe87c00660
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-11-05 19:31:40 -0500

    removed css. changed html buttons to divs and spans.

---
+++ main.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector('.calculator')
+const keys = document.querySelector('.keys')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')){
+        
+    }
+})
